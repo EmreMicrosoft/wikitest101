e1afcf47458fcef1d473c08f269230e6c9d29626
Juan Vilar Sanchis <Juan Vilar Sanchis>
2017-05-12 06:27:03 +0200

    first upload

---
+++ js/wiki.js
@@ -0,0 +1,32 @@
+var langArray = ["es","hi","ru","en","fr", "jp", "ne","ca" ,"ar", "ka"];
+var langValue = ["Spanish", "Hindi", "Russian", "English", "French", "Japanese", "Nepali", "Catalan", "Arab", "Georgian"];
+var selectedlang = 0;
+
+function getWikiContent(title)
+{
+	var xmlHttp = null;
+	var theUrl = "http://" + langArray[selectedlang] + ".wikipedia.org/w/api.php?action=parse&format=json&page=" + title;
+
+    xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+    xhr.setRequestHeader("Origin", "http://wiki.innovastudios.net");
+    xhr.open( "GET", theUrl, true );
+    xhr.send();
+    document.getElementById("content").innerHtml=xhr.text;
+}
+
+
+var timeout= null
+
+function startSearch(text)
+{
+	if (timeout == null)
+	{
+		timeout = setTimeout(function(){getWikiContent(text)});
+	}
+	else
+	{
+		clearTimeout(timeout);
+		timeout = setTimeout(function(){getWikiContent(text)});
+	}
+}
